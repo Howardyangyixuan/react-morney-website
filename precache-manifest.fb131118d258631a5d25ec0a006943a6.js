990cc834e67de45c511904ca2a4da1a58e1d442f
howardyangyixuan <howardyangyixuan@gmail.com>
2021-02-08 17:47:28 +0800

    deploy

---
+++ precache-manifest.fb131118d258631a5d25ec0a006943a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2d8fcbd7fa04a9fa141beadb09d6b3",
+    "url": "/react-morney-website/index.html"
+  },
+  {
+    "revision": "0a385cefc1ef091d2193",
+    "url": "/react-morney-website/static/css/main.f0f767ca.chunk.css"
+  },
+  {
+    "revision": "a222ac058ac30294881f",
+    "url": "/react-morney-website/static/js/2.d3636ed3.chunk.js"
+  },
+  {
+    "revision": "4648a5ac342ec5040769b374fc01c4fd",
+    "url": "/react-morney-website/static/js/2.d3636ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a385cefc1ef091d2193",
+    "url": "/react-morney-website/static/js/main.49c89a8b.chunk.js"
+  },
+  {
+    "revision": "0afbf527da0cb27e3f99",
+    "url": "/react-morney-website/static/js/runtime-main.5c78bade.js"
+  }
+]);
